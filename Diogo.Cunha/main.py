6935ccf998de7e7bf83c0e1a8fa03fb24ba027a3
namelew <diogomaciel.cunha@gmail.com>
2022-07-09 21:37:39 -0300

    create pheromone table

--- Diogo.Cunha/main.py
+++ Diogo.Cunha/main.py
@@ -31,38 +31,43 @@ class OutputInstancia: # classe par a organizar os dados de saida
 def createInterval(n:int) -> int: # retorna por quantos segundos uma instância será executada
     return round((n * 60)/1000, 0)
 
-def exploreNode(instancia:list, nodo:int, unvisited:list, tam:int): # explora um nodo escolhido
-    sample = []
-
-    unvisited.sort(key=lambda x:instancia[nodo][x])
-
-    sampleTam = round(tam * 0.3)
-
-    for i in unvisited:
-        if(len(sample) == sampleTam):
-            break
-        sample.append(i)
+def chooseNewNode(pheroTable:list, nodo:int) -> int:
+    pSum = 0
+    for i in range(len(pheroTable)):
+        pSum += pheroTable[nodo][i]
     
-    drawn = choice(sample)
+    pRange = 0
+    drawn = randint(0, maxsize) % pSum
 
-    while (drawn == nodo):
-        drawn = choice(sample)
-
-    unvisited.remove(drawn)
+    i = 0
+    for pV in pheroTable[nodo]:
+        if drawn > pRange and drawn < (pRange + pV):
+            return i
+        pRange += pV
+        i += 1
+        
+def exploreNode(instancia:list, nodo:int, pheroTable:list, tam:int): # explora um nodo escolhido
+    i = 0
+    for distance in instancia[nodo]:
+        if i != nodo:
+            if pheroTable[nodo][i] == 0 or pheroTable[i][nodo] == 0: 
+                pheroTable[nodo][i] = tam/distance
+                pheroTable[i][nodo] = tam/distance
+        i += 1
+    
+    drawn = chooseNewNode(pheroTable, nodo)
 
     return instancia[nodo][drawn], drawn
 
-def BCCF(instancia:list, tam:int): # algoritmo principal da busca gulosa
-    unvisited = [i for i in range(tam)] # controla nodos que ainda não foram visitados
+def BCCF(instancia:list, pheroTable:list,tam:int): # algoritmo principal da busca gulosa
     solution = []
 
     current = randint(0, tam-1)
-    unvisited.remove(current)
     solution.append(current)
     best = 0
 
-    while(len(unvisited) > 0):
-        custo,current = exploreNode(instancia, current, unvisited, tam)
+    while(len(solution) < tam):
+        custo,current = exploreNode(instancia, current, pheroTable, tam)
         solution.append(current)
         best += custo
     
@@ -84,14 +89,17 @@ for file in files: # carregando a matrix de adjacência
 for i in range(len(files)):
     tam = len(instancias[i])
     output.append(OutputInstancia(files[i]))
+    pheroTable = []
+    for j in range(tam):
+        pheroTable.append([0 for k in range(tam)])
     print(f"Starting {output[i].name}")
   
     for max in range(10):
         start = time()
         best = maxsize
-        bestPath = []
+        bestPath = [0]
         while(abs(start - time()) < createInterval(tam)):
-            solution,current = BCCF(instancias[i], tam)
+            solution,current = BCCF(instancias[i], pheroTable, tam)
             if current < best:
                 best = current
                 bestPath = solution

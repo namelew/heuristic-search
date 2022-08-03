814f67cde66462fd0aed379274f6313d84409249
namelew <diogomaciel.cunha@gmail.com>
2022-08-02 22:05:37 -0300

    implementing genetic algoritm

--- Diogo.Cunha/main.py
+++ Diogo.Cunha/main.py
@@ -1,9 +1,8 @@
+from random import randint, sample,choice
 import numpy as np
-from random import choice, randint
-from sys import maxsize
 from time import time
 import pandas as pd
-from math import isinf
+
 class OutputInstancia: # classe par a organizar os dados de saida
     def __init__(self, name:str):
         self.name = name
@@ -30,90 +29,165 @@ class OutputInstancia: # classe par a organizar os dados de saida
         return round(sum/tam, 0)
 
 def createInterval(n:int) -> int: # retorna por quantos segundos uma instância será executada
-    return round((n * 60)/1000, 0)
+    return round((n * 180)/1000, 0)
 
-def chooseNewNode(pheroTable:list, unvisited:set, nodo:int, tam:int) -> int: # seleciona a nova cidade baseada em seu feromônio
-    samples = []
+def generateTamPopulation(n:int) -> int: # se size não for par, arrendonda para cima até o par mais próximo
+    size = round(n ** 0.5)
+    if size%2 != 0:
+        while(size%2 != 0):
+            size+=1
+        return size
+    return size
 
-    for i in range(tam):
-        if i in unvisited:
-            samples.append(i)
-
-    pSum = 0
-    for sample in samples:
-        pSum += pheroTable[nodo][sample]
-    
-    if pSum != 0:
-        pRange = 0
-        drawn = randint(0, pSum)
-        for sample in samples:
-            if i in unvisited and pRange <= drawn <= (pRange+pheroTable[nodo][sample]):
-                return sample
-            pRange += pheroTable[nodo][sample]
-    opt = []
-    for i in range(tam):
-        if i in unvisited:
-            opt.append(i)
-    drawn = choice(opt)
-
-    return drawn
-
-def exploreNode(instancia:list, nodo:int, pheroTable:list, unvisited:set, tam:int): # explora um nodo escolhido
-    i = 0
-    for distance in instancia[nodo]:
-        if i in unvisited:
-            newPhero = pheroTable[nodo][i]/distance
-            if not isinf(newPhero * 1000) or newPhero <= 4000:
-                aux = round(newPhero * 1000, 2)
-                pheroTable[nodo][i] = round(aux)
-                pheroTable[i][nodo] = round(aux)
-            else:
-                pheroTable[nodo][i] = 4000
-                pheroTable[i][nodo] = 4000
-        i += 1
-    
-    drawn = chooseNewNode(pheroTable, unvisited, nodo, tam)
-    unvisited.remove(drawn)
-
-    return instancia[nodo][drawn], drawn
-
-def BCCF(instancia:list, pheroTable:list,tam:int): # algoritmo principal da busca por colônia de formigas
-    solution = []
+def crossoverOperator(inst:list, father:list, mother:list, tam:int, start:int) -> list:
+    son = [-1 for _ in range(tam)]
     unvisited = set()
-    
-    current = randint(0, tam-1)
 
     for i in range(tam):
-        if i != current:
-            unvisited.add(i)
+        unvisited.add(i)
+
+    son[0] = father[start]
+    son[1] = father[start+1]
+    unvisited.remove(father[start])
+    unvisited.remove(father[start+1])
     
-    solution.append(current)
-    best = 0
+    for i in range(2, tam):
+        a = (mother.index(son[i-1]) + 1) % tam
+        b = (father.index(son[i-1]) + 1) % tam 
 
-    while(len(unvisited) > 0):
-        custo,current = exploreNode(instancia, current, pheroTable, unvisited, tam)
-        solution.append(current)
-        best += custo
+        menor = a
+        if inst[son[i - 1]][b] < inst[son[i - 1]][menor]:
+            menor = b
+        
+        son[i] = menor
+        
+        if son[i] not in unvisited:
+            a = choice(list(unvisited))
+            b = choice(list(unvisited))
+
+            if inst[son[i - 1]][b] < inst[son[i - 1]][menor]:
+                son[i] = b
+            else:
+                son[i] = a
+
+        unvisited.remove(son[i])
+ 
+    return son
+
+def AEX(father:list, mother:list, tam:int) -> list:
+  son = [-1 for _ in range(tam)]
+  unvisited = set()
+  for i in range(tam):
+    unvisited.add(i)
+
+  for i in range(2):
+    son[i] = father[i]
+    unvisited.remove(father[i])
+
+  parents = [father, mother]
+  
+  for i in range(2, tam):
+    son[i] = parents[1][(parents[1].index(son[i-1]) + 1) % tam]
     
-    return solution,best
+    if son[i] not in unvisited:
+      son[i] = choice(list(unvisited))
 
-def reinforcePhero(pheroTable:list, solution:list, tam:int): # fortalece os feromônios de todos os nodos que aparecem na solução
-    for i in range(tam - 1):
-        pheroTable[solution[i]][solution[i + 1]] = pheroTable[solution[i]][solution[i + 1]] * 1.2
-        pheroTable[solution[i + 1]][solution[i]] = pheroTable[solution[i + 1]][solution[i]] * 1.2
-    pheroTable[solution[i]][solution[0]] = pheroTable[solution[i]][solution[0]] * 1.2
-    pheroTable[solution[0]][solution[i]] = pheroTable[solution[0]][solution[i]] * 1.2
+    unvisited.remove(son[i])
+    
+    parents.reverse()
+  return son
 
-def diminishPhero(pheroTable:list, solution:list, tam:int): # enfraquece os feromônios
-    for i in range(tam - 1):
-        pheroTable[solution[i]][solution[i + 1]] = pheroTable[solution[i]][solution[i + 1]] * 0.8
-        pheroTable[solution[i + 1]][solution[i]] = pheroTable[solution[i + 1]][solution[i]] * 0.8
-    pheroTable[solution[i]][solution[0]] = pheroTable[solution[i]][solution[0]] * 0.8
-    pheroTable[solution[0]][solution[i]] = pheroTable[solution[0]][solution[i]] * 0.8
+def CX(father:list, mother:list, tam:int) -> list:
+  son = [-1 for _ in range(tam)]
+
+  for i in range(tam):
+    if father[i] not in son:
+      son[i] = father[i]
+    else:
+      break
+    if mother[i] not in son:
+      son[father.index(mother[i])] = mother[i]
+    else:
+      break
+  
+  for i in range(tam):
+    if mother[i] not in son:
+      son[son.index(-1)] = mother[i]
+  
+  return son
+
+def swap(array:list, a:int, b:int):
+    temp = array[a]
+    array[a] = array[b]
+    array[b] = temp
+
+def mutation(sons:list, tam:int, ti:int):
+    selected = sample(range(tam), round(tam * 0.2))
+
+    for i in selected:
+        a = randint(0, ti - 1)
+        b = randint(0, ti - 1)
+        while b == a:
+            b = randint(0, ti - 1)
+        swap(sons[i], a, b)
+
+def initPopulation(n:int) ->list:
+    population = []
+    for _ in range(generateTamPopulation(n)):
+        population.append(sample(range(n), n))
+    return population
+
+def getCusto(instancia:list, sample:list, tam: int) -> int:
+    custo = 0
+    for i in range(tam-1):
+        custo += instancia[sample[i]][sample[i+1]]
+    return custo
+
+def AGHGreX(instancia:list, population:list, tam:int) -> list:
+    tp = len(population)
+    sons = []
+    # selecionando reprodutores
+    breeders = sample(range(tp), tp)
+    # etapa reprodutiva
+    for i in range(int(tp/2)):
+        bst1  = randint(0, tam-3)
+        sons.append(crossoverOperator(instancia, population[breeders[i]], population[breeders[i+1]], tam, bst1))
+
+        bst2  = randint(0, tam-3)
+        while bst2 == bst1:
+            bst2  = randint(0, tam-3)
+        sons.append(crossoverOperator(instancia, population[breeders[i+2]], population[breeders[i+3]], tam, bst2))
+    
+    mutation(sons, tp, tam)
+    # seleção natural
+    tempP = []
+    for i in range(tp):
+        tempP.append((getCusto(instancia, population[i], tam), i))
+    tempP.sort(key=lambda x:x[0])
+
+    for i in range(round(tp * 0.8)):
+        tempP.pop()
+    
+    tempS = []
+    for i in range(tp):
+        tempS.append((getCusto(instancia, sons[i], tam), i))
+    tempS.sort(key=lambda x:x[0])
+
+    for i in range(round(tp * 0.2)):
+        tempS.pop()
+    
+    new_pop = []
+    for a in tempP:
+        new_pop.append(population[a[1]])
+    for b in tempS:
+        new_pop.append(sons[b[1]])
+    
+    return new_pop
 
 instancias = []
-files = ["Western Sahara", "Djibouti",  "Qatar",  "Uruguay", "Zimbabwe"]
-li = [29, 38, 194, 734, 929] # tamanho de cada uma das instâncias
+files = ["Western Sahara", "Djibouti",  "Qatar"]
+li = [29, 38, 194] # tamanho de cada uma das instâncias
+
 output = []
 for file in files: # carregando a matrix de adjacência
     array = np.fromfile("Instâncias/"+file+".bin")
@@ -127,29 +201,23 @@ for file in files: # carregando a matrix de adjacência
 for i in range(len(files)):
     tam = len(instancias[i])
     output.append(OutputInstancia(files[i]))
-    pheroTable = [] # tabela de mapeamento de feromônios
-    for j in range(tam):
-        pheroTable.append([tam if j != k else 0 for k in range(tam)])
     print(f"Starting {output[i].name}")
     for max in range(10):
         start = time()
-        best = maxsize
-        bestPath = [0]
+        population = initPopulation(tam)
         while(abs(start - time()) < createInterval(tam)):
-            solution,current = BCCF(instancias[i], pheroTable, tam)
-            if current < best:
-                reinforcePhero(pheroTable, solution, tam)
-                best = current
-                bestPath = solution
-            else:
-                diminishPhero(pheroTable, solution, tam)
+            population = AGHGreX(instancias[i], population, tam)
         output[i].time.append(abs(start-time()))
-        output[i].solutions.append(best)
+        tempP = []
+        for j in range(generateTamPopulation(tam)):
+            tempP.append(getCusto(instancias[i], population[j], tam))
+        tempP.sort()
+        output[i].solutions.append(tempP[0])
 # gera a saída no arquivo resultados.csv
 dist_to_csv = {
     "instancia": [data.name for data in output],
-    "autoria": ["Diogo.Cunha" for i in range(len(output))],
-    "algoritmo": ["BCCF" for i in range(len(output))],
+    "autoria": ["Diogo.Cunha" for _ in range(len(output))],
+    "algoritmo": ["AGHGreX" for _ in range(len(output))],
     "q-medio": [int(data.avgQ()) for data in output],
     "q-desvio": [f"{data.dispersionQ():.02f}" for data in output],
     "t-medio": [int(data.avgT()) for data in output]
